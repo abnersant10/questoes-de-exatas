67694d02163de7e2a7f74cadfc237ab71e993158
Abner Santana <abnersant10@gmail.com>
2022-07-06 15:02:17 -0300

    modelo de banco de dados
    definido as models para o usuário e para as questões com os parametros da TRI : a, b e c.

--- core/models.py
+++ core/models.py
@@ -1,4 +1,29 @@
 from django.db import models
-from django.forms import CharField
+from django.forms import BooleanField, CharField, ChoiceField, DateField, EmailField, FloatField
 
 # Create your models here.
+
+
+class usuario(models.Model):
+    nome = CharField(max_length=100)
+    sobrenome = CharField(max_length=100)
+    email = EmailField()
+    nascimento = DateField()
+    genero = CharField(max_length=50)
+    perfil = CharField(max_length=50)
+    teta = FloatField()
+
+
+class questao(models.Model):
+    nome = CharField(max_length=100)
+    curso = CharField(max_length=100)
+    disciplina = CharField(max_length=100)
+    assunto = CharField(max_length=100)
+    ano = CharField(max_length=20)
+    multidisciplinar = BooleanField()
+    a = FloatField()
+    b = FloatField()
+    c = FloatField()
+    enunciado = CharField(max_length=4096)
+    alternativas = ChoiceField()  # Adicionar as alternativas
+    solucao = CharField(max_length=4096)
