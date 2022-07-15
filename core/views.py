31728bb271f219416d1bcfd5fac48fe91763e561
Abner Santana <abnersant10@gmail.com>
2022-07-15 14:12:44 -0300

    front-end e back-end de logine cadastro
    Pagina inicial do front end alterado, pagina de autocadastro alterado

--- core/views.py
+++ core/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.contrib.auth import *
 
 # Create your views here.
+
+
 def home(request):
-    return render(request, "index.html")
+    # se usuário esta autenticado vá para pagina inicial ####
+    if request.method == 'POST':
+        email = str(request.POST.get('email'))
+        senha = request.POST.get('senha')
+        user = authenticate(request, username=email, password=senha)
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            print("Senha invalida ?")
+    else:
+        return render(request, "index.html")
+
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
--- questoesexatas/urls.py
+++ questoesexatas/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import home
+from core.views import home, cadastro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('cadastro', cadastro, name='cadastro')
 ]
