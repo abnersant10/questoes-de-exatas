d3ae32e907da89c87a8c74a05ba6634b7a5f8459
Abner <abnersant10@gmail.com>
2022-07-30 10:41:47 -0300

    atualizar models
    modelagem do banco de dados na classe models

--- core/models.py
+++ core/models.py
@@ -4,17 +4,8 @@ from django.forms import BooleanField, CharField, ChoiceField, DateField, EmailF
 
 # Create your models here.
 
-
 class questao(models.Model):
-    cod = models.AutoField(primary_key=True)
-    curso = models.CharField(max_length=100)
-    disciplina = models.CharField(max_length=100)
-    assunto = models.CharField(max_length=100)
-    ano = models.IntegerField()
-    # parametros da TRI
-    a = models.FloatField()
-    b = models.FloatField()
-    c = models.FloatField()
+    cod_q = models.AutoField(primary_key=True)
     enunciado = models.TextField()
     alt_a = models.TextField()
     alt_b = models.TextField()
@@ -22,10 +13,24 @@ class questao(models.Model):
     alt_d = models.TextField()
     alt_e = models.TextField()
     gabarito = models.CharField(max_length=1)
-    solucao = models.TextField()
 
     def __str__(self):
         return str(self.cod)
+
+class questao_parametro(models.Model):
+    cod_qp = models.OneToOneField(questao, on_delete=models.CASCADE)
+    a = models.FloatField()
+    b = models.FloatField()
+    c = models.FloatField()
+
+class questao_catacteristica(models.Model):
+    cod_qc = models.ForeignKey(questao, on_delete=models.CASCADE, default=None, related_name='fk')
+    curso = models.CharField(max_length=45)
+    banca = models.CharField(max_length=45)
+    disciplina = models.CharField(max_length=45)
+    assunto = models.CharField(max_length=45)
+    ano = models.IntegerField()
+
 
 
 class teta_usuario(models.Model):
