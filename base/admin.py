728e16cafaefdaa173daa990024d833d4c9ad985
Abner <abnersant10@gmail.com>
2022-08-10 16:44:07 -0300

    Adicionando models no admin
    Adicionei os campos da model no admin do Django.

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class AreaAdmin(admin.ModelAdmin):
+
+    list_display = ('nome_area', 'cod_area')
+
+
+class CursoAdmin(admin.ModelAdmin):
+
+    list_display = ('nome_curso', 'cod_curso', 'area_cod')
+
+
+class DisciplinaAdmin(admin.ModelAdmin):
+
+    list_display = ('nome_disciplina', 'cod_disciplina', 'curso_cod', 'area_cod')
+
+
+class AssuntoAdmin(admin.ModelAdmin):
+
+    list_display = ('assunto', 'cod_assunto', 'disciplina_cod', 'curso_cod', 'area_cod')
+
+
+class TetaUsuarioAdmin(admin.ModelAdmin):
+
+    list_display = ('usuario', 'teta', 'disciplina_cod', 'assunto_cod')
+
+
+class QuestaoAdmin(admin.ModelAdmin):
+
+    list_display = ('cod_questao', 'enunciado', 'banca_examinadora', 'ano_divulgacao', 'curso_cod', 'assunto_cod')
+
+
+class QuestaoAlternativaAdmin(admin.ModelAdmin):
+
+    list_display = ('questao_cod', 'A', 'B', 'C', 'D', 'E', 'gabarito')
+
+
+class QuestaoParametroAdmin(admin.ModelAdmin):
+
+    list_display = ('questao_cod', 'A', 'B', 'C')
+
+
+admin.site.register(Area, AreaAdmin)
+admin.site.register(Curso, CursoAdmin)
+admin.site.register(Disciplina, DisciplinaAdmin)
+admin.site.register(Assunto, AssuntoAdmin)
+admin.site.register(TetaUsuario, TetaUsuarioAdmin)
+admin.site.register(Questao, QuestaoAdmin)
+admin.site.register(QuestaoAlternativa, QuestaoAlternativaAdmin)
+admin.site.register(QuestaoParametro, QuestaoParametroAdmin)
--- base/models.py
+++ base/models.py
@@ -8,36 +8,38 @@ class Area(models.Model):
     nome_area = models.CharField(max_length=90)
 
 
+
+
 class Curso(models.Model):
 
     cod_curso = models.CharField(primary_key=True, max_length=45)
     nome_curso = models.CharField(max_length=90)
-    area_cod = models.ForeignKey(Area, "Cod Area", on_delete=models.CASCADE)
+    area_cod = models.ForeignKey(Area, on_delete=models.CASCADE)
 
 
 class Disciplina(models.Model):
 
     cod_disciplina = models.CharField(primary_key=True, max_length=45)
     nome_disciplina = models.CharField(max_length=90)
-    curso_cod = models.ForeignKey(Curso, "Cod Curso", on_delete=models.CASCADE)
-    area_cod = models.ForeignKey(Area, "Cod Area", on_delete=models.CASCADE)
+    curso_cod = models.ForeignKey(Curso,on_delete=models.CASCADE)
+    area_cod = models.ForeignKey(Area,on_delete=models.CASCADE)
 
 
 class Assunto(models.Model):
 
     cod_assunto = models.CharField(primary_key=True, max_length=45)
     assunto = models.CharField(max_length=90)
-    disciplina_cod = models.ForeignKey(Disciplina, "Cod Disciplina", on_delete=models.CASCADE)
-    curso_cod = models.ForeignKey(Curso, "Cod Curso", on_delete=models.CASCADE)
-    area_cod = models.ForeignKey(Area, "Cod Area", on_delete=models.CASCADE)
+    disciplina_cod = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    curso_cod = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    area_cod = models.ForeignKey(Area, on_delete=models.CASCADE)
 
 
 class TetaUsuario(models.Model):
     teta = models.FloatField()
     usuario = models.ForeignKey(
         User, on_delete=models.CASCADE, default=None, related_name='fk')
-    disciplina_cod = models.ForeignKey(Disciplina, "Cod Disciplina", on_delete=models.CASCADE)
-    assunto_cod = models.ForeignKey(Assunto, "Cod Assunto", on_delete=models.CASCADE)
+    disciplina_cod = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    assunto_cod = models.ForeignKey(Assunto, on_delete=models.CASCADE)
 
 
 class Questao(models.Model):
@@ -45,9 +47,9 @@ class Questao(models.Model):
     enunciado = models.TextField()
     banca_examinadora = models.CharField(max_length=45)
     ano_divulgacao = models.CharField(max_length=45)
-    area_cod = models.ForeignKey(Area, "Cod Area", on_delete=models.CASCADE)
-    curso_cod = models.ForeignKey(Curso, "Cod Curso", on_delete=models.CASCADE)
-    assunto_cod = models.ForeignKey(Assunto, "Cod Assunto", on_delete=models.CASCADE)
+    area_cod = models.ForeignKey(Area, on_delete=models.CASCADE)
+    curso_cod = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    assunto_cod = models.ForeignKey(Assunto, on_delete=models.CASCADE)
 
 
 class QuestaoAlternativa(models.Model):
@@ -57,7 +59,7 @@ class QuestaoAlternativa(models.Model):
     D = models.TextField()
     E = models.TextField()
     gabarito = models.CharField(max_length=1)
-    questao_cod = models.OneToOneField(Questao, "Cod Questao", on_delete=models.CASCADE)
+    questao_cod = models.OneToOneField(Questao, on_delete=models.CASCADE)
 
 
 class QuestaoParametro(models.Model):
@@ -65,5 +67,4 @@ class QuestaoParametro(models.Model):
     A = models.TextField()
     B = models.TextField()
     C = models.TextField()
-    questao_cod = models.OneToOneField(Questao, "Cod Questao", on_delete=models.CASCADE)
-
+    questao_cod = models.OneToOneField(Questao, on_delete=models.CASCADE)
