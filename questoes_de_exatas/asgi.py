802ea6fc056afc0eff08546d8ca32504558f37a9
Abner <abnersant10@gmail.com>
2022-08-10 12:05:55 -0300

    RECRIANDO A ESTRUTURA BASICA
    projeto inicial, recriando a estrutura basica do ambiente Django

---
+++ questoes_de_exatas/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for questoes_de_exatas project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'questoes_de_exatas.settings')
+
+application = get_asgi_application()
---
+++ questoes_de_exatas/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for questoes_de_exatas project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'questoes_de_exatas.settings')
+
+application = get_wsgi_application()
