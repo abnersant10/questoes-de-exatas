65c36c8aaf9f0f57d480bfbd6c06903e9a07bd48
Abner <abnersant10@gmail.com>
2022-08-10 13:08:19 -0300

    update estrutura basica de pesquisa

--- base/views.py
+++ base/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import *
+from django.contrib.auth.models import User
+import json
+import pathlib
 
-# Create your views here.
+def home(request):
+
+    if request.method == 'POST':
+        email = str(request.POST.get('email'))
+        senha = request.POST.get('senha')
+        user = authenticate(request, username=email, password=senha)
+        if user is not None:
+            login(request, user)
+            return redirect("pagina-inicial")
+    if request.user.is_authenticated:
+        return redirect("pagina-inicial")
+    else:
+        return render(request, "index.html")
+
+
+def logout_view(request):
+
+    logout(request)
+    return redirect('/')
+
+
+def cadastro(request):
+
+    if request.method == 'POST':
+        nome = str(request.POST.get('nome'))
+        sobrenome = str(request.POST.get('sobrenome'))
+        email = str(request.POST.get('email'))
+        senha = str(request.POST.get('senha'))
+        User.objects.create_user(
+            username=email, first_name=nome, last_name=sobrenome, password=senha)
+    return render(request, 'cadastro.html')
+
+
+def pag_inicial(request):
+
+    return render(request, 'pagina-inicial.html')
+
+
+def consultar(request):
+
+    folder = json.load(
+        open(str(pathlib.Path().resolve())+'\\base\\folder.json', encoding='utf8'))
+    context = {
+        "folder": folder,
+    }
+    return render(request, 'consultar.html', context)
--- questoes_de_exatas/urls.py
+++ questoes_de_exatas/urls.py
@@ -1,21 +1,11 @@
-"""questoes_de_exatas URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-
+from base.views import home, cadastro, pag_inicial, consultar, logout_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('cadastro', cadastro, name='cadastro'),
+    path('pagina-inicial', pag_inicial, name='pagina-inicial'),
+    path('consultar-questoes', consultar, name='consultar'),
+    path('logout', logout_view, name="logout"),
 ]
