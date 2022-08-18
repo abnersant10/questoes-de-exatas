9f73245c1bd537f79fea8d4a7d053ef97116131b
Abner <abnersant10@gmail.com>
2022-08-18 19:19:20 -0300

    se usuario nao esta autenticado retorna pra login
    se o usuário não estiver autenticado o teste adaptativo não vai funcionar

--- base/views.py
+++ base/views.py
@@ -56,104 +56,106 @@ def consultar_quest(request):
 
 def nav_quest(request, assunto,disciplina,):
 
+    if request.user.is_authenticated:
+        #BUSCA
+        disciplina = disciplina.replace('-', ' ')
+        assunto = assunto.replace('-', ' ')
+        disciplina_select = Disciplina.objects.filter(nome_disciplina__contains=disciplina)
+        assunto_select = Assunto.objects.filter(assunto__contains=assunto)
+        itens_select = Questao.objects.filter(assunto_cod='CE01CCAS01')
+        itens_param = []
+        tot_itens = len(itens_select)
+        teta = 2 # inicialmente
+        #itens_usados = []
+        vetor_respostas = []
+        vetor_param = []
 
-    #BUSCA
-    disciplina = disciplina.replace('-', ' ')
-    assunto = assunto.replace('-', ' ')
-    disciplina_select = Disciplina.objects.filter(nome_disciplina__contains=disciplina)
-    assunto_select = Assunto.objects.filter(assunto__contains=assunto)
-    itens_select = Questao.objects.filter(assunto_cod='CE01CCAS01')
-    itens_param = []
-    tot_itens = len(itens_select)
-    teta = 2 # inicialmente
-    #itens_usados = []
-    vetor_respostas = []
-    vetor_param = []
-
-    for item in itens_select:
-        a = QuestaoParametro.objects.get(questao_cod=item.cod_questao).A
-        b = QuestaoParametro.objects.get(questao_cod=item.cod_questao).B
-        c = QuestaoParametro.objects.get(questao_cod=item.cod_questao).C
-        d = QuestaoParametro.objects.get(questao_cod=item.cod_questao).D
-        param_i = []
-        param_i.append(a)
-        param_i.append(b)
-        param_i.append(c)
-        param_i.append(d)
-        vetor_param.append((param_i))
-        #print(vetor_param)
-    vetor_param = array(vetor_param)
-
-
+        for item in itens_select:
+            a = QuestaoParametro.objects.get(questao_cod=item.cod_questao).A
+            b = QuestaoParametro.objects.get(questao_cod=item.cod_questao).B
+            c = QuestaoParametro.objects.get(questao_cod=item.cod_questao).C
+            d = QuestaoParametro.objects.get(questao_cod=item.cod_questao).D
+            param_i = []
+            param_i.append(a)
+            param_i.append(b)
+            param_i.append(c)
+            param_i.append(d)
+            vetor_param.append((param_i))
+            #print(vetor_param)
+        vetor_param = array(vetor_param)
 
 
-    item = UrrySelector()
-    itens_usados = request.POST.get('id_itens')
-    print(itens_usados)
-    if itens_usados == None:
-        itens_usados = ''
-    itens_usados = [int(i) for i in itens_usados.split()]
-
-    #itens_usados = [1]
-    print(itens_usados)
-    i_novo_item = item.select(items=vetor_param, administered_items=itens_usados, est_theta=teta)
-    banca = itens_select[int(i_novo_item)].banca_examinadora
-    ano = itens_select[int(i_novo_item)].ano_divulgacao
-    enunciado = itens_select[int(i_novo_item)].enunciado
-    cod_questao = itens_select[int(i_novo_item)].cod_questao
-    alt_a = QuestaoAlternativa.objects.get(questao_cod=cod_questao).A
-    alt_b = QuestaoAlternativa.objects.get(questao_cod=cod_questao).B
-    alt_c = QuestaoAlternativa.objects.get(questao_cod=cod_questao).C
-    alt_d = QuestaoAlternativa.objects.get(questao_cod=cod_questao).D
-    alt_e = QuestaoAlternativa.objects.get(questao_cod=cod_questao).E
-    gabarito = QuestaoAlternativa.objects.get(questao_cod=cod_questao).gabarito
-    param_a = QuestaoParametro.objects.get(questao_cod=cod_questao).A
-    param_b = QuestaoParametro.objects.get(questao_cod=cod_questao).B
-    param_c = QuestaoParametro.objects.get(questao_cod=cod_questao).C
-    itens_usados.append((i_novo_item))
 
 
-    # receber e tratar os dados do vetor respostas
-    user_resp = request.POST.get('user_resp')
-    server_vet_resp = request.POST.get('vet_resp')
-    if server_vet_resp == None:
-        server_vet_resp = ''
-    if user_resp == gabarito:
-        server_vet_resp = server_vet_resp + '1'
-    elif user_resp != None:
-        server_vet_resp = server_vet_resp + '0'
-
-    # receber e tratar os itens administrados
-    #tot_itens = 6
-    server_id_items = request.POST.get('server_id_items')
-
-
-    if server_id_items == None:
-        server_id_items = ''
-    if len(itens_usados) < tot_itens:
-        itens_usados = " ".join(map(str, itens_usados))
-        server_id_items = itens_usados + ' ' 
+        item = UrrySelector()
+        itens_usados = request.POST.get('id_itens')
         print(itens_usados)
+        if itens_usados == None:
+            itens_usados = ''
+        itens_usados = [int(i) for i in itens_usados.split()]
 
-    context = {
-        'assunto' : assunto_select,
-        'disciplina': disciplina_select,
-        'itens_selec' : itens_select,
-        'enunciado' : enunciado,
-        'ano':ano,
-        'banca': banca,
-        'alt_a' : alt_a,
-        'alt_b': alt_b,
-        'alt_c': alt_c,
-        'alt_d': alt_d,
-        'alt_e': alt_e,
-        'gabarito' : gabarito,
-        'param_a' : param_a,
-        'param_b': param_b,
-        'param_c': param_c,
-        'teta': teta,
-        'server_vet_resp': server_vet_resp,
-        'server_id_items' : server_id_items,
+        #itens_usados = [1]
+        print(itens_usados)
+        i_novo_item = item.select(items=vetor_param, administered_items=itens_usados, est_theta=teta)
+        banca = itens_select[int(i_novo_item)].banca_examinadora
+        ano = itens_select[int(i_novo_item)].ano_divulgacao
+        enunciado = itens_select[int(i_novo_item)].enunciado
+        cod_questao = itens_select[int(i_novo_item)].cod_questao
+        alt_a = QuestaoAlternativa.objects.get(questao_cod=cod_questao).A
+        alt_b = QuestaoAlternativa.objects.get(questao_cod=cod_questao).B
+        alt_c = QuestaoAlternativa.objects.get(questao_cod=cod_questao).C
+        alt_d = QuestaoAlternativa.objects.get(questao_cod=cod_questao).D
+        alt_e = QuestaoAlternativa.objects.get(questao_cod=cod_questao).E
+        gabarito = QuestaoAlternativa.objects.get(questao_cod=cod_questao).gabarito
+        param_a = QuestaoParametro.objects.get(questao_cod=cod_questao).A
+        param_b = QuestaoParametro.objects.get(questao_cod=cod_questao).B
+        param_c = QuestaoParametro.objects.get(questao_cod=cod_questao).C
+        itens_usados.append((i_novo_item))
 
-    }
-    return render(request, 'nav-quest.html', context)
+
+        # receber e tratar os dados do vetor respostas
+        user_resp = request.POST.get('user_resp')
+        server_vet_resp = request.POST.get('vet_resp')
+        if server_vet_resp == None:
+            server_vet_resp = ''
+        if user_resp == gabarito:
+            server_vet_resp = server_vet_resp + '1'
+        elif user_resp != None:
+            server_vet_resp = server_vet_resp + '0'
+
+        # receber e tratar os itens administrados
+        #tot_itens = 6
+        server_id_items = request.POST.get('server_id_items')
+
+
+        if server_id_items == None:
+            server_id_items = ''
+        if len(itens_usados) < tot_itens:
+            itens_usados = " ".join(map(str, itens_usados))
+            server_id_items = itens_usados + ' '
+            print(itens_usados)
+
+        context = {
+            'assunto' : assunto_select,
+            'disciplina': disciplina_select,
+            'itens_selec' : itens_select,
+            'enunciado' : enunciado,
+            'ano':ano,
+            'banca': banca,
+            'alt_a' : alt_a,
+            'alt_b': alt_b,
+            'alt_c': alt_c,
+            'alt_d': alt_d,
+            'alt_e': alt_e,
+            'gabarito' : gabarito,
+            'param_a' : param_a,
+            'param_b': param_b,
+            'param_c': param_c,
+            'teta': teta,
+            'server_vet_resp': server_vet_resp,
+            'server_id_items' : server_id_items,
+
+        }
+        return render(request, 'nav-quest.html', context)
+    else:
+        return redirect('home')
